0e13fa2ee0f5dd7f78771d99e1c013e5d991af7c
sergeylyashko <slyashko@mail.ru>
2023-03-25 09:43:08 +0300

    update pom

--- src/main/java/me/t/gilllepulla/functional/patterns/bridge/Bridge.java
+++
@@ -1,9 +0,0 @@
-package me.t.gilllepulla.functional.patterns.bridge;
-
-import java.util.function.Consumer;
-
-@FunctionalInterface
-public interface Bridge<T> {
-
-    void action(Consumer<T> consumer);
-}
